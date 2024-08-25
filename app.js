38d8424cad3b06d2e665de5f7e2f2f00ad488bfb
Rupe88 <chyrupesh828@gmail.com>
2024-08-25 11:48:23 +0545

    dev

--- app.js
+++ app.js
@@ -19,8 +19,8 @@ app.use(express.json({ limit: "100mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-  origin: "https://adhd-awareness-frontend.vercel.app",
-  credentials: true, // Important for allowing cookies across domains
+  origin:"http://localhost:5173",
+  credentials: true, 
 }));
 
 // Testing endpoint
